d4a39d93bbb8061f097d3a5b3582ac5f695d6907
Pennuel <paulpennuel@gmail.com>
2022-08-24 08:27:35 +0300

    this was a working version of the app need to add proper translation

---
+++ app/src/main/java/com/example/tensorflowtranslator/viewmodel/TranslatorViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.tensorflowtranslator.viewmodel
+
+import android.content.ActivityNotFoundException
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.ViewModel
+import com.example.tensorflowtranslator.R
+
+class TranslatorViewModel: ViewModel() {
+    companion object {
+        const val TAG = "TFLite - ODT"
+        const val REQUEST_IMAGE_CAPTURE: Int = 1
+        private const val MAX_FONT_SIZE = 96F
+    }
+
+    /**
+     * onClick(v: View?)
+     *      Detect touches on the UI components
+     */
+
+}
